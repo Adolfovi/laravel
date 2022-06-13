ca373e018e279f19a0706d76f395076f3f47b937
Adolfovi <dolfillo99@gmail.com>
2022-06-13 12:06:41 +0200

    Mejoración general del código

--- resources/js/front/desktop/product.js
+++ resources/js/front/desktop/product.js
@@ -3,18 +3,14 @@ export let renderProducts = () => {
     let mainContainer = document.querySelector("main");
     let viewButtons = document.querySelectorAll('.view-product');
     let categoryButtons = document.querySelectorAll('.category-button');
-
     document.addEventListener("renderProductModules",( event =>{
         renderProducts();
     }), {once: true});
-
-    
     viewButtons.forEach(viewButton => {
 
         viewButton.addEventListener('click', () => {
 
             let url = viewButton.dataset.url;
-
             let sendShowRequest = async () => {
 
                 let response = await fetch(url, {
@@ -51,11 +47,8 @@ export let renderProducts = () => {
     categoryButtons.forEach(categoryButton => {
 
         categoryButton.addEventListener('click', () => {
-
             let url = categoryButton.dataset.url;
-
             let sendShowRequest = async () => {
-
                 let response = await fetch(url, {
                     headers: {
                         'X-Requested-With': 'XMLHttpRequest',
@@ -63,7 +56,6 @@ export let renderProducts = () => {
                     method: 'GET', 
                 })
                 .then(response => {
-                                
                     if (!response.ok) throw response;
 
                     return response.json();
@@ -83,7 +75,6 @@ export let renderProducts = () => {
             };
 
             sendShowRequest();
-            
         });
     });
     
