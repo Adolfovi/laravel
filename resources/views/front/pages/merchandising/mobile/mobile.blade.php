3c90eed6f16ba3a727889480cb4d61dc3abc4e66
Adolfovi <dolfillo99@gmail.com>
2022-06-13 15:44:55 +0200

    Mejoración del código responsive

--- resources/views/front/pages/merchandising/mobile/mobile.blade.php
+++ resources/views/front/pages/merchandising/mobile/mobile.blade.php
@@ -5,78 +5,60 @@
                 <div class="category-merchans-title">
                     <h2>@yield('page-title')</h2>
                 </div>
-                <form action="/merchan.php">
-                    <select name="type-products" id="type-products">
-                        <option value="todos"><span>Todos</span></option>
-                        <option value="camisetas"><span>Camisetas</span></option>
-                        <option value="gorras"><span>Gorras</span></option>
-                        <option value="fundas-movil"><span>Fundas de móvil</span></option>
-                        <option value="relojes"><span>Relojes</span></option>
+               
+                <div class="category-merchans-content">
+                    <ul>
+                        @if(isset($product_categories))
+                            @foreach($product_categories as $category_element)
+                                <li class="category-button" data-url="{{route('front_category_product', ['category' => $category_element->id])}}"><b>{{$category_element->title}}</b></li>
+                            @endforeach
+                        @endif
+                    </ul>
+                    @if(isset($products))
+                    <span>Ordenar por:</span>
+                    <br>
+                    <select name="orderBy" class="orderBy">
+                        <option value=""> </option>
+                        <option value="{{route('front_orderby_product', ['order' => 'ASC'])}}">Precio Ascendente</option>
+                        <option value="{{route('front_orderby_product', ['order' => 'DESC'])}}">Precio Descendente</option>
                     </select>
-                </form>
+                    @endif
+                </div>        
             </div>
         </div>
         <div class="column-main">
             <div class="results-products-container">
                 <div class="results-products-info">
                     <div class="results-products-total">
-                        <span>Se están enseñando 20 de 100 productos</span>
+                        <span>Se están enseñando {{$products->count()}} producto/s</span>
                     </div>
                     <div class="results-products-selector">
                     </div>
                 </div>
                 <div class="results-products-elements">
-                    <div class="element">
-                        <div class="element-image">
-                            <img src="img/reloj.png">
-                        </div>
-                        <div class="element-price">
-                            <span>14.99€</span>
-                        </div>
-                        <div class="element-name">
-                            <span>Reloj Espartacoin</span>
-                        </div>
-                        <div class="element-see">
-                            <button>
-                                <a href="merchan-product.html">Ver producto</a>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="element">
-                        <div class="element-image"><img src="img/reloj.png"></div>
-                        <div class="element-price"><span>14.99€</span></div>
-                        <div class="element-name"><span>Reloj Espartacoin</span></div>
-                        <div class="element-see"><button><a href="merchan-product.html">Ver
-                                    producto</a></button></div>
-                    </div>
-                    <div class="element">
-                        <div class="element-image"><img src="img/reloj.png"></div>
-                        <div class="element-price"><span>14.99€</span></div>
-                        <div class="element-name"><span>Reloj Espartacoin</span></div>
-                        <div class="element-see"><button><a href="merchan-product.html">Ver
-                                    producto</a></button></div>
-                    </div>
-                    <div class="element">
-                        <div class="element-image"><img src="img/reloj.png"></div>
-                        <div class="element-price"><span>14.99€</span></div>
-                        <div class="element-name"><span>Reloj Espartacoin</span></div>
-                        <div class="element-see"><button><a href="merchan-product.html">Ver
-                                    producto</a></button></div>
-                    </div>
-                    <div class="element">
-                        <div class="element-image"><img src="img/reloj.png"></div>
-                        <div class="element-price"><span>14.99€</span></div>
-                        <div class="element-name"><span>Reloj Espartacoin</span></div>
-                        <div class="element-see"><button><a href="merchan-product.html">Ver
-                                    producto</a></button></div>
-                    </div>
-                    <div class="element">
-                        <div class="element-image"><img src="img/reloj.png"></div>
-                        <div class="element-price"><span>14.99€</span></div>
-                        <div class="element-name"><span>Reloj Espartacoin</span></div>
-                        <div class="element-see"><button><a href="merchan-product.html">Ver
-                                    producto</a></button></div>
-                    </div>
+                    @if(isset($products))
+                        @foreach($products as $product)
+                            <div class="element">
+                                <div class="element-image">
+                                    <img src="img/reloj.png">
+                                </div>
+                                <div class="element-price">
+                                    <span>{{$product->price}}€</span>
+                                </div>
+                                <div class="element-name">
+                                    <span>{{$product->name}}</span>
+                                </div>
+                                <div class="element-categoryname">
+                                    <span>{{$product->category_name}}</span>
+                                </div>
+                                <div class="element-see">
+                                    <button class="view-product" data-url="{{route('front_product', ['product' => $product->id])}}">
+                                        Ver producto
+                                    </button>
+                                </div>
+                            </div>  
+                        @endforeach
+                    @endif                    
                 </div>
             </div>
         </div>
