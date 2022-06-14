2bce6ad8b015e8d44205cf3be5e389decd0bc2be
Adolfovi <dolfillo99@gmail.com>
2022-06-14 14:24:27 +0200

    Inserción de nuevas tablas

---
+++ app/Models/Payment_Method.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment_Method extends Model
+{
+    protected $guarded = [];
+    protected $table = 'payment_methods';
+}
