00049bd94fe2321173681b5f418f31e61cd375c0
Adolfovi <dolfillo99@gmail.com>
2022-06-28 15:47:04 +0200

    Inserción de nuevos elementos en el panel de ventas

--- resources/views/admin/pages/ventas/index.blade.php
+++ resources/views/admin/pages/ventas/index.blade.php
@@ -1,13 +1,20 @@
 @extends('admin.layout.table_form')
 @section('content')
+
+
 <h3>DATOS DE LA VENTA:</h3>
 <h4>Número de ticket</h4>
+<p>75685721323</p>
 <h4>Fecha de creación</h4>
+<p>12/07/2022</p>
 <h4>Método de pago</h4>
+<p>Master Card</p>
 <h4>Total Base</h4>
+<p>1.20</p>
 <h4>Total IVA</h4>
+<p>56.00</p>
 <h4>Total</h4>
-
+<p>259.00</p>
 
 
 
