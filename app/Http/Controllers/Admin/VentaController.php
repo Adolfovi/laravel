7e96331f969b958232452e5d44c490ca83716e6b
Adolfovi <dolfillo99@gmail.com>
2022-06-29 11:42:05 +0200

    Modificación del controlador de ventas

--- app/Http/Controllers/Admin/VentaController.php
+++ app/Http/Controllers/Admin/VentaController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Cart;
 use App\Http\Requests\Admin\UserRequest;
 use Debugbar;
 
 
 
 class VentaController extends Controller {
+    protected $cart;
 
 
-
-public function __construct(){
-
+public function __construct(Cart $cart){
+    $this->cart = $cart;
 }
 
 
@@ -25,8 +26,17 @@ public function __construct(){
 
 
 
+
 public function index(){
-    return view('admin.pages.ventas.index');
+    return view('admin.pages.ventas.index')
+    ->with('carts',$this->cart->where('carts.active', 1)
+    ->select(DB::raw('*, products.name as product, payment_methods.name as pmethod'))
+    ->join('ventas','carts.venta_id', '=', 'ventas.id')
+    ->join('payment_methods','ventas.payment_method_id', '=', 'payment_methods.id')
+    ->join('prices','carts.price_id', '=', 'prices.id')
+    ->join('products','prices.product_id', '=', 'products.id')    
+    ->get());
+    
 }
 
 public function show(){}
--- resources/views/admin/pages/ventas/index.blade.php
+++ resources/views/admin/pages/ventas/index.blade.php
@@ -7,16 +7,36 @@
 <h3>DATOS DE LA VENTA:</h3>
 <br>
 <br>
+@if(isset($carts))
+@foreach ($carts as $cart)
 <h4>Número de ticket</h4>
-<p>75685721323</p>
+<p>{{$cart->ticket_number}}</p>
 <h4>Fecha de creación</h4>
-<p>12/07/2022</p>
+<p>{{$cart->date_emision}}</p>
 <h4>Método de pago</h4>
-<p>Master Card</p>
+<p>{{$cart->pmethod}}</p>
 <h4>Total Base</h4>
-<p>1.20€</p>
+<p>{{$cart->total_base_price}}€</p>
 <h4>Total IVA</h4>
-<p>56.00€</p>
+<p>{{$cart->total_tax_price}}€</p>
 <h4>Total</h4>
-<p>259.00€</p>
+<p>{{$cart->total_base_price + $cart->total_tax_price}}€</p>
+<table border="1">
+    <tr>
+<td></td>
+<td>Producto</td>
+<td>Precio</td>
+<td>Cantidad</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>{{$cart->product}}</td>
+<td></td>
+<td></td>
+    </tr>
+</table>
+<br>
+<br>
+@endforeach
+@endif
 @endsection
